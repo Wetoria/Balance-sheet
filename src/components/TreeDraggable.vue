f29c7c68c212075c9e1f5d74d96d0a5f3b332a75
Wetoria <vwetoria@gmail.com>
2019-12-12 23:11:05 +0800

    Add slot

--- src/components/TreeDraggable.vue
+++ src/components/TreeDraggable.vue
@@ -1,67 +1,26 @@
 <template>
   <el-tree
     :data="data"
-    node-key="id"
+    :props="{
+      label: 'name',
+    }"
+    node-key="key"
     default-expand-all
     draggable
   >
+    <template v-slot="{ node, data }" v-if="$slots">
+      <slot v-bind:data="data" v-bind:node="node"></slot>
+    </template>
   </el-tree>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      data: [{
-        id: 1,
-        label: '一级 1',
-        children: [{
-          id: 4,
-          label: '二级 1-1',
-          children: [{
-            id: 9,
-            label: '三级 1-1-1',
-          }, {
-            id: 10,
-            label: '三级 1-1-2',
-          }],
-        }],
-      }, {
-        id: 2,
-        label: '一级 2',
-        children: [{
-          id: 5,
-          label: '二级 2-1',
-        }, {
-          id: 6,
-          label: '二级 2-2',
-        }],
-      }, {
-        id: 3,
-        label: '一级 3',
-        children: [{
-          id: 7,
-          label: '二级 3-1',
-        }, {
-          id: 8,
-          label: '二级 3-2',
-          children: [{
-            id: 11,
-            label: '三级 3-2-1',
-          }, {
-            id: 12,
-            label: '三级 3-2-2',
-          }, {
-            id: 13,
-            label: '三级 3-2-3',
-          }],
-        }],
-      }],
-      defaultProps: {
-        children: 'children',
-        label: 'label',
-      },
-    };
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
   },
   methods: {
   },
